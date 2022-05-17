95eef78a9e46bd46716e49e4ff1e3705b070131e
Mohamed Ibrahim <100520743+M0hamedIbrahim1@users.noreply.github.com>
2022-05-17 23:00:33 +0200

    Create mutations.py

---
+++ Functions/mutations.py
@@ -0,0 +1,13 @@
+// link : https://www.hackerrank.com/challenges/python-mutations/problem
+// author : @Mohamed Ibrahim
+
+def mutate_string(string, position, character):
+    s = list(string)
+    s[position] = character
+    return ''.join(s)
+
+if __name__ == '__main__':
+    s = input()
+    i, c = input().split()
+    s_new = mutate_string(s, int(i), c)
+    print(s_new)
