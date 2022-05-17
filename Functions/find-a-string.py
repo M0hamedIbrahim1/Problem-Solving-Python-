7b23c3295ca788c34f1866f8b929530e36a3f167
Mohamed Ibrahim <100520743+M0hamedIbrahim1@users.noreply.github.com>
2022-05-18 01:14:06 +0200

    Create find-a-string.py

---
+++ Functions/find-a-string.py
@@ -0,0 +1,16 @@
+#link : https://www.hackerrank.com/challenges/find-a-string/problem
+#Author : @Mohamed Ibrahim
+
+def count_substring(string, sub_string):
+    count = 0
+    for i in range(len(string)):
+        if string[i:].startswith(sub_string):
+            count = count+1
+    return count
+
+if __name__ == '__main__':
+    string = input().strip()
+    sub_string = input().strip()
+    
+    count = count_substring(string, sub_string)
+    print(count)
