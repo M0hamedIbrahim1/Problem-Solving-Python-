cb533c322882c481b1df6b620086f99def10bab8
Mohamed Ibrahim <100520743+M0hamedIbrahim1@users.noreply.github.com>
2022-06-17 23:18:34 +0200

    Update Collections.namedtuple().py

--- Public Problems/Collections.namedtuple().py
+++ Public Problems/Collections.namedtuple().py
@@ -1,6 +1,20 @@
 #link : https://www.hackerrank.com/challenges/py-collections-namedtuple/problem
 #author : Mohamed Ibrahim
 
+from collections import namedtuple
+
+n_students = int(input())
+header = input().split()
+students = namedtuple('student',header)
+summation = 0
+for i in range(n_students):
+    inp_1, inp_2, inp_3,inp_4 = input().split()
+    student = students(inp_1, inp_2, inp_3,inp_4)
+    summation += int(student.MARKS)
+print('{:.2f}'.format(summation/n_students))
+
+
+
 n = int(input())
 summation = 0
 input_1 = input().split()
