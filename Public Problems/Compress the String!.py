d6e1b838919c7e87fb578b12d54fa68b6f1c27d9
Mohamed Ibrahim <100520743+M0hamedIbrahim1@users.noreply.github.com>
2022-06-21 22:21:15 +0200

    Update Compress the String!.py

--- Public Problems/Compress the String!.py
+++ Public Problems/Compress the String!.py
@@ -2,3 +2,30 @@ from itertools import groupby
 List=input()
 for i, items in groupby(List):    
     print(tuple([len(list(items)),int(i)]), end=' ')
+
+   -------------------------------- 
+
+# from itertools import groupby
+# N = input()
+# for k,g in groupby(N):
+#     print(list(k))  
+    
+# 1222311
+['1']
+['2']
+['3']
+['1']
+   --------------------------------- 
+    
+# from itertools import groupby
+# N = input()
+# for k,g in groupby(N):
+#     print(list(g))
+
+# 1222311
+
+['1']
+['2', '2', '2']
+['3']
+['1', '1']
+-------------------------------------
