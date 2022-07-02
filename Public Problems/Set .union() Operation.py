4340a715f98552346b81505a333971c144139467
Mohamed Ibrahim <100520743+M0hamedIbrahim1@users.noreply.github.com>
2022-07-02 20:23:43 +0200

    Create Set .union() Operation.py

---
+++ Public Problems/Set .union() Operation.py
@@ -0,0 +1,6 @@
+#link   : https://www.hackerrank.com/challenges/py-set-union/problem
+#author : Mohamed Ibrahim
+
+s1,a = input(),set(input().split())
+s2,b = input(),set(input().split())
+print(len(a.union(b)))
