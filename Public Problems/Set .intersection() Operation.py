f044b949b2ce652e627120423b42bdc35c2fecf5
Mohamed Ibrahim <100520743+M0hamedIbrahim1@users.noreply.github.com>
2022-07-02 21:04:09 +0200

    Create Set .intersection() Operation.py

---
+++ Public Problems/Set .intersection() Operation.py
@@ -0,0 +1,6 @@
+#link   : https://www.hackerrank.com/challenges/py-set-intersection-operation/problem 
+#authoe : Mohamed Ibrahim
+
+s1,a = input(),set(input().split())
+s2,b = input(),set(input().split())
+print(len(a.intersection(b)))
