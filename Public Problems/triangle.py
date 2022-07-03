1e798e247e2522ef38e0f215694eea82d8b29ca2
Mohamed Ibrahim <100520743+M0hamedIbrahim1@users.noreply.github.com>
2022-07-03 21:38:39 +0200

    Create triangle.py

---
+++ Public Problems/triangle.py
@@ -0,0 +1,10 @@
+#link :   https://www.hackerrank.com/challenges/triangle-quest-2/problem
+#author : Mohamed Ibrahim
+for i in range(1,int(input())+1):
+    print(int('1' * i)**2)
+
+    
+# for i in range(1,int(input())+1):
+#     a_list = list(range(1,i+1))
+#     final_list = a_list + sorted(a_list,reverse=True)[1:]
+#     print(*final_list,' ')
