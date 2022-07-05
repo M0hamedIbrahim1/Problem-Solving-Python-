aea14e49fc61580840debe4ddf470d38a81f381b
Mohamed Ibrahim <100520743+M0hamedIbrahim1@users.noreply.github.com>
2022-07-05 21:17:13 +0200

    Create Maximize It!.py

---
+++ Public Problems/Maximize It!.py
@@ -0,0 +1,9 @@
+# link : https://www.hackerrank.com/challenges/maximize-it/problem
+author : Mohamed Ibrahim
+
+from itertools import product
+
+K,M = map(int,input().split())
+N = (list(map(int, input().split()))[1:] for _ in range(K))
+results = map(lambda x: sum(i**2 for i in x)%M, product(*N))
+print(max(results))
