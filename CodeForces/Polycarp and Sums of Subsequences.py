ce6b9137cc454d32415bcb510b8b88c6c86380f0
Mohamed Ibrahim <100520743+M0hamedIbrahim1@users.noreply.github.com>
2022-07-05 14:07:33 +0200

    Create Polycarp and Sums of Subsequences.py

---
+++ CodeForces/Polycarp and Sums of Subsequences.py
@@ -0,0 +1,9 @@
+#link   : https://codeforces.com/problemset/problem/1618/A
+#author : Mohamed Ibrahim
+
+t= int(input())
+for i in range(t):
+   lst=list(map(int,input().split()))
+   print(lst[0],lst[1],lst[5]-lst[1])
+  
+  
