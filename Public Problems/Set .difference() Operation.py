5e55fee8c8b8a88d2379a5aa1e612e0c5a790829
Mohamed Ibrahim <100520743+M0hamedIbrahim1@users.noreply.github.com>
2022-07-05 21:22:06 +0200

    Update Set .difference() Operation.py

--- Public Problems/Set .difference() Operation.py
+++ Public Problems/Set .difference() Operation.py
@@ -1,3 +1,6 @@
+#link : https://www.hackerrank.com/challenges/py-set-difference-operation/problem
+#author : Mohamed Ibrahim
+
 n , a = input(),set(map(int,input().split()))
 N , b = input(),set(map(int,input().split()))
 print(len(a.difference(b)))
