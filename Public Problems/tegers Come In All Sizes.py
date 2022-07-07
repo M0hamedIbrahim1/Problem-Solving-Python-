a5a1e4a16e43719de009d4717f1acf724002772d
Mohamed Ibrahim <100520743+M0hamedIbrahim1@users.noreply.github.com>
2022-07-07 20:12:39 +0200

    Create tegers Come In All Sizes.py

---
+++ Public Problems/tegers Come In All Sizes.py
@@ -0,0 +1,4 @@
+#link  : https://www.hackerrank.com/challenges/python-integers-come-in-all-sizes/problem
+#author : Mohamed Ibrahim
+a,b,c,d = int(input()),int(input()),int(input()),int(input())
+print(pow(a,b) + pow(c,d))
