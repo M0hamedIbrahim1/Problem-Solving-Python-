2614ae9f7bce19130a8ef71c9f83323cd58f0cbb
Mohamed Ibrahim <100520743+M0hamedIbrahim1@users.noreply.github.com>
2022-07-13 23:05:26 +0200

    Create eval.py

---
+++ Public Problems/eval.py
@@ -0,0 +1,14 @@
+# The eval() expression is a very powerful built-in function of Python. It helps in evaluating an expression. The expression can be a Python statement, or a code object.
+
+# For example:
+
+# >>> eval("9 + 5")
+# 14
+# >>> x = 2
+# >>> eval("x + 3")
+# 5
+
+
+#link   : https://www.hackerrank.com/challenges/python-eval/problem
+#author : Mohamed Ibrahim
+eval(input())
