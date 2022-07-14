321b31f9e382f104eba4784ddcee0c6c79c5bf8e
Mohamed Ibrahim <100520743+M0hamedIbrahim1@users.noreply.github.com>
2022-07-14 21:33:28 +0200

    Create Detect Floating Point Number.py

---
+++ Public Problems/Detect Floating Point Number.py
@@ -0,0 +1,8 @@
+#link   :  https://www.hackerrank.com/challenges/introduction-to-regex/problem
+#author : Mohamed Ibrahim
+
+for _ in range(int(input())):
+    try:
+        print(bool(float(input())))
+    except:
+        print("False")
