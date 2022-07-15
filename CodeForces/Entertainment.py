19088f77611d5366f50f8525a60917329aed1850
Mohamed Ibrahim <100520743+M0hamedIbrahim1@users.noreply.github.com>
2022-07-15 19:51:58 +0200

    Create Entertainment.py

---
+++ CodeForces/Entertainment.py
@@ -0,0 +1,6 @@
+#link   : https://codeforces.com/group/jfviGllBoY/contest/389763/problem/G
+#author: Mohamed Ibrahim
+  
+import math
+a,b = map(int,input().split())
+print(math.factorial(min(a,b)))
