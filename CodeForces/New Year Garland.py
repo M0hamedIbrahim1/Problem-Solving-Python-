3617b5789faa0d75d1c2b59e1f9dae9ef6959d3f
Mohamed Ibrahim <100520743+M0hamedIbrahim1@users.noreply.github.com>
2022-08-01 20:15:05 +0200

    Create  New Year Garland.py

---
+++ CodeForces/New Year Garland.py
@@ -0,0 +1,6 @@
+#link   : https://codeforces.com/problemset/problem/1279/A
+#author : Mohamed Ibrahim
+
+for t in range(int(input())):
+	a = sorted(list(map(int, input().split())))
+	print('Yes' if a[2] <= a[0] + a[1] + 1 else 'No'
