da642ca85f2f6c497a203c6f46c726591f298aea
Mohamed Ibrahim <100520743+M0hamedIbrahim1@users.noreply.github.com>
2022-08-05 18:38:18 +0200

    Create A. Bark to Unlock.py

---
+++ CodeForces/A. Bark to Unlock.py
@@ -0,0 +1,10 @@
+#link : https://codeforces.com/problemset/problem/868/A
+#author : Mohamed Ibrahim
+
+c,v0,v1,a,l=map(int,input().split())
+sum1=v0
+days=1
+while(sum1<c):
+    sum1+=min(v0+days*a-l,v1-l)
+    days+=1
+print(days)
