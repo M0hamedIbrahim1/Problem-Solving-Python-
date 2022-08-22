46bc4add3769fee83a837c995a529c6eb6c9e851
Mohamed Ibrahim <100520743+M0hamedIbrahim1@users.noreply.github.com>
2022-08-22 20:18:23 +0200

    Create A. Subtract or Divide.py

---
+++ CodeForces/A. Subtract or Divide.py
@@ -0,0 +1,8 @@
+#link   : https://codeforces.com/problemset/problem/1451/A
+#author : Mohamed Ibrahim
+
+for i in range (int(input())):
+    n=int(input())
+    print(min(n - 1, (n % 2) + 2))
+    
+   
