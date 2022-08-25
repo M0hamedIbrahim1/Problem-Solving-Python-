a42a3f1de8a83305181bfe23d00f57ca21195585
Mohamed Ibrahim <100520743+M0hamedIbrahim1@users.noreply.github.com>
2022-08-25 22:27:28 +0200

    Create C. Infinity Table.py

---
+++ CodeForces/C. Infinity Table.py
@@ -0,0 +1,12 @@
+https://codeforces.com/problemset/problem/1560/C
+
+for _ in range(int(input())):
+  a = int(input())
+  k = 1
+  while k*k < a:
+    k += 1
+  p = k*k
+  if p - k + 1 <= a:
+    print(k, p-a+1)
+  else:
+    print(k-((p-k+1)-a), k)
