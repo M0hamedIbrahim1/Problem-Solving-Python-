7a914fdef13d1551858cb35d6c947db37f965c19
Mohamed Ibrahim <100520743+M0hamedIbrahim1@users.noreply.github.com>
2022-08-27 23:18:40 +0200

    Create A. Two Regular Polygons.py

---
+++ CodeForces/A. Two Regular Polygons.py
@@ -0,0 +1,7 @@
+# link : https://codeforces.com/problemset/problem/1312/A
+# author : Mohamed Ibrahim
+
+for _ in range(int(input())):
+    a,b=map(int,input().split())
+    print("YES" if a%b==0 else "NO")
+    
