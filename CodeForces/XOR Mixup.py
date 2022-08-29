4886f35b802759ca2e17a9d2d026dac18a9b3e88
Mohamed Ibrahim <100520743+M0hamedIbrahim1@users.noreply.github.com>
2022-08-29 20:27:09 +0200

    Create  XOR Mixup.py

---
+++ CodeForces/XOR Mixup.py
@@ -0,0 +1,9 @@
+# link  : https://codeforces.com/problemset/problem/1698/A
+# author : Mohamed Ibrahim
+
+y=int(input())
+for i in range(y):
+    l=int(input())
+    y=input().split()
+    print(y[0])
+    
