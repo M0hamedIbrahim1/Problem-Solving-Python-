6f1f715a1c2d618efc3502a9f8d8964d48c6db30
Mohamed Ibrahim <100520743+M0hamedIbrahim1@users.noreply.github.com>
2022-09-08 21:44:42 +0200

    Create A. Choose Two Numbers.py

---
+++ CodeForces/A. Choose Two Numbers.py
@@ -0,0 +1,8 @@
+# link : https://codeforces.com/problemset/problem/1206/A
+# author : Mohamed Ibrahim
+
+input()
+a=map(int,input().split())
+input()
+b=map(int,input().split())
+print(max(a),max(b))
