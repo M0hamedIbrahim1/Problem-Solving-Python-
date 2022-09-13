12172bbe070b1e97e45c2534cd8d5b59128c449f
Mohamed Ibrahim <100520743+M0hamedIbrahim1@users.noreply.github.com>
2022-09-13 22:20:43 +0200

    Create A. Permutation Forgery.py

---
+++ CodeForces/A. Permutation Forgery.py
@@ -0,0 +1,6 @@
+# link   : https://codeforces.com/problemset/problem/1405/A
+# author : Mohamed Ibrahim
+
+for t in range(int(input())):
+	n = int(input())
+	print(' '.join(reversed(input().split())))
