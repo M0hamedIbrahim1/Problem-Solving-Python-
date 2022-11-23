7308972b026d79fd5a70478417beb409bf996ba1
Mohamed Ibrahim <100520743+M0hamedIbrahim1@users.noreply.github.com>
2022-11-23 22:05:40 +0200

    Create A. Medium Number.py

---
+++ CodeForces/A. Medium Number.py
@@ -0,0 +1,11 @@
+# link   : https://codeforces.com/contest/1760/problem/A
+# author : Mohamed Ibrahim
+
+t = int(input())
+for _ in range(t):
+  l = list(map(int,input().split()))
+  l.sort()
+  print(l[1])
+  
+  
+  
