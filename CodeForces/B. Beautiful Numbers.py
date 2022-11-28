3c2dce97657211b3c5903b88c7b4b6c6bc550091
Mohamed Ibrahim <100520743+M0hamedIbrahim1@users.noreply.github.com>
2022-11-28 02:01:33 +0200

    Create B. Beautiful Numbers.py

---
+++ CodeForces/B. Beautiful Numbers.py
@@ -0,0 +1,19 @@
+# link   : https://codeforces.com/problemset/problem/1265/B
+# author : Mohamed Ibrahim
+
+
+for _ in range(int(input())):
+    n = int(input())
+    *a, = map(int, input().split())
+    arr = [(a[i], i) for i in range(n)]
+    arr.sort()
+    l = r = arr[0][1]
+    ans = [1]*n
+    for i in range(1, n):
+        l = min(l, arr[i][1])
+        r = max(r, arr[i][1])
+        ans[i] = int(r-l==i)
+    print(*ans,sep='')
+
+    
+    
