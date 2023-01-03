83ea7b1bcd0e5d326d46e3a7cac2431210295e9f
Mohamed Ibrahim <100520743+M0hamedIbrahim1@users.noreply.github.com>
2023-01-03 14:18:24 +0200

    Create Data Structure ProbArray Average.py

---
+++ Data Structure ProbArray Average.py
@@ -0,0 +1,14 @@
+# link   : https://codeforces.com/group/MWSDmqGsZm/contest/223339/problem/S
+# author : Mohamed Ibrahim
+
+
+def resc(lst,sum):
+  if len(lst) == 0:
+    return sum
+  sum += lst[0]
+  return resc(lst[1:],sum)
+ 
+n = int(input())
+lst =list(map(int,input().split()))
+print(resc(lst,0)/n) 
+
