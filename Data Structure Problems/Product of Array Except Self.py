a8c0c8e5b828fc88b758a33c94dcbcdb523cc9bc
Mohamed Ibrahim <100520743+M0hamedIbrahim1@users.noreply.github.com>
2023-01-27 12:08:20 +0200

    Create Product of Array Except Self.py

---
+++ Data Structure Problems/Product of Array Except Self.py
@@ -0,0 +1,18 @@
+# link   : https://leetcode.com/problems/product-of-array-except-self/solutions/
+# author : Mohamed Ibrahim
+
+
+
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        ans = [1]*len(nums)
+        prefix_sum = 1
+        for i in range(1,len(nums)):
+            ans[i] = ans[i-1]*nums[i-1]
+        for i in range(len(nums)-1,-1,-1):
+            ans[i]*=prefix_sum
+            prefix_sum*=nums[i]
+        return ans
+      
+      
+      
