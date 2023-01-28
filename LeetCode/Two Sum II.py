0469cdf9289470ae00f8e1edc251a700cd3c3d95
Mohamed Ibrahim <100520743+M0hamedIbrahim1@users.noreply.github.com>
2023-01-28 21:19:00 +0200

    Update Two Sum II.py

--- LeetCode/Two Sum II.py
+++ LeetCode/Two Sum II.py
@@ -10,5 +10,17 @@ class Solution:
             if numbers[i]+numbers[j] > target:j-=1
             else:i+=1
             
+
             
-            
+# Using Recursion
+
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        return self.resc(numbers,0,len(numbers)-1,target)
+    def resc(self,numbers,i,j,t):
+        sum = numbers[i]+numbers[j]
+        if sum == t: return [i+1,j+1]
+        elif sum > t:
+            return self.resc(numbers,i,j-1,t)
+        else:
+            return self.resc(numbers,i+1,j,t)            
