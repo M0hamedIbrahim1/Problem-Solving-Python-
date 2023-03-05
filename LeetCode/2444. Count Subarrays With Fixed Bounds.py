b0bed76d9722373a4c2d66c91c2303dfa6892d6e
Mohamed Ibrahim <100520743+M0hamedIbrahim1@users.noreply.github.com>
2023-03-05 23:31:49 +0200

    Create 2444. Count Subarrays With Fixed Bounds.py

---
+++ LeetCode/2444. Count Subarrays With Fixed Bounds.py
@@ -0,0 +1,14 @@
+# link   : https://leetcode.com/problems/count-subarrays-with-fixed-bounds/description/
+# author : Mohamed Ibrahim
+
+    def countSubarrays(self, A: List[int], minK: int, maxK: int) -> int:
+        res = 0
+        jmin = jmax = jbad = -1
+        for i,a in enumerate(A):
+            if not minK <= a <= maxK: jbad = i
+            if a == minK: jmin = i
+            if a == maxK: jmax = i
+            res += max(0, min(jmin, jmax) - jbad)
+        return res
+        
+        
