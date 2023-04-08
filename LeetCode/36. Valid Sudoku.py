a83c7398af06683a6a2af9f115bfe5dd7c52bcbb
Mohamed Ibrahim <100520743+M0hamedIbrahim1@users.noreply.github.com>
2023-04-08 13:55:21 +0200

    Create 36. Valid Sudoku.py

---
+++ LeetCode/36. Valid Sudoku.py
@@ -0,0 +1,22 @@
+# link   : https://leetcode.com/problems/valid-sudoku/description/
+# author : Mohamed Ibrahim
+
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        rows = defaultdict(set)
+        cols = defaultdict(set)
+        squares = defaultdict(set)
+
+        for i in range(9):
+            for j in range(9):
+                if board[i][j] == ".":
+                    continue
+                if  (board[i][j] in rows[i] or 
+                    board[i][j] in cols[j] or
+                    board[i][j] in squares[(i//3,j//3)]):
+                        return False
+                rows[i].add(board[i][j])               
+                cols[j].add(board[i][j])
+                squares[(i//3,j//3)].add(board[i][j])
+        return True
+        
