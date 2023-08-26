eb90e7c272e2d8b2b1461fddb61e3675398a9276
Mohamed Ibrahim <100520743+M0hamedIbrahim1@users.noreply.github.com>
2023-08-26 06:56:42 +0300

    Create 646. Maximum Length of Pair Chain.py

---
+++ LeetCode/646. Maximum Length of Pair Chain.py
@@ -0,0 +1,14 @@
+# https://leetcode.com/problems/maximum-length-of-pair-chain/description/
+
+class Solution:
+    def findLongestChain(self, pairs: List[List[int]]) -> int:
+        # Sort pairs in ascending order based on the second element.
+        pairs.sort(key=lambda x: x[1])
+        curr = float('-inf')
+        ans = 0
+
+        for pair in pairs:
+            if pair[0] > curr:
+                ans += 1
+                curr = pair[1]
+        return ans
